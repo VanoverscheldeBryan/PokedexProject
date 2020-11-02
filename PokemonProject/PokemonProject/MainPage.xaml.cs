55ca703b0231403bed545f2d7d013426b788d48c
Bryan <bryan.vanoverschelde@student.howest.be>
2020-11-02 22:29:32 +0100

    Beginner navigation buttons created, added stylesheet for css styling and navigates to new view

---
+++ PokemonProject/PokemonProject/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using PokemonProject.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PokemonProject
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Kanto_Clicked(object sender, EventArgs e)
+        {
+            NavigateToKanto();
+        }
+
+        private void NavigateToKanto()
+        {
+            Navigation.PushAsync(new KantoPage());
+
+        }
+
+        private void Johto_Clicked(object sender, EventArgs e)
+        {
+            NavigateToJohto();
+        }
+
+        private void NavigateToJohto()
+        {
+            Navigation.PushAsync(new JohtoPage());
+
+        }
+
+        private void Hoenn_Clicked(object sender, EventArgs e)
+        {
+            NavigateToHoenn();
+        }
+
+        private void NavigateToHoenn()
+        {
+            Navigation.PushAsync(new HoennPage());
+
+        }
+    }
+}
---
+++ PokemonProject/PokemonProject/Views/JohtoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PokemonProject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class JohtoPage : ContentPage
+    {
+        public JohtoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
